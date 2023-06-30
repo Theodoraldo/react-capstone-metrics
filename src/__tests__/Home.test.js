3a409ee7795648d6d44900e248cd044320701f20
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-06-30 03:31:02 +0000

    Remoce errors and fixed failed test

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -2,11 +2,47 @@ import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import Home from '../components/Home';
 
-test('renders the home component snapshot', () => {
-  const { asFragment } = render(
+describe('Renders correctly', () => {
+  test('renders the home component snapshot', () => {
+    const { asFragment } = render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>,
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('object assignment', () => {
+    render(      
     <BrowserRouter>
       <Home />
     </BrowserRouter>,
-  );
-  expect(asFragment()).toMatchSnapshot();
-});
+    );
+
+    const data = {one: 1};
+    data['two'] = 2;
+    data['three'] = 3;
+    expect(data).toEqual({one: 1, two: 2, three: 3});
+  })
+
+  test('object assignment', () => {
+    render(      
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>,
+    );
+
+    const continentList = [
+      'Africa',
+      'America',
+      'Antarctic',
+      'Asia',
+      'Europe',
+      'Oceania',
+    ];
+
+    const num = continentList.length;
+    expect(num).toEqual(6);
+    expect(continentList).toContain('Africa');
+  })
+})
